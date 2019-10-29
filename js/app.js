dc04fcddd053649462e5b5b219d7c493478217cb
xflatiboy <xflati@inbox.ru>
2019-10-29 08:59:59 +0300

    Ну это финал, босс, как бы усе.
    Ну ты понимаешь, да? тут нету полезной инфы, зато будет крутой текст, мол, мы так близки, мммм... Наконец то, скоро отдохнешь от меня и моей ручной писанины(ctrl+c ctrl+v придумал лентяй, все рукам, все хардкором, так ошибок больше, так круче, как настоящий мужик)

--- js/app.js
+++ js/app.js
@@ -1,20 +1,20 @@
 var buttons = document.querySelectorAll('.services-flex .button');
-var slides = document.querySelectorAll('.service-flex');
+var slides = document.querySelectorAll('.service-slide');
 
 [].slice.call(buttons).forEach( function(item) {
-	item.addEventListener("click", function(evt) {
+	item.addEventListener('click', function(evt) {
 		[].slice.call(buttons).forEach( function(_item) {
-			_item.classList.remove("is-active");
+			_item.classList.remove('is-active');
 		});
 
-		evt.target.classList.add("is-active");
+		evt.target.classList.add('is-active');
 		var slideId = evt.target.dataset.slide;
 		var slide = document.querySelector('#'+slideId);
 			[].slice.call(slides).forEach( function(_slide) {
-				_slide.classList.remove("is-active");
+				_slide.classList.remove('is-active');
 			})
 
-			slide.classList.add("is-active");
+			slide.classList.add('is-active');
 	});
 });
 
@@ -25,13 +25,13 @@ var message = document.querySelector('#message-us');
 var mapButton = document.querySelector('#map-handler')
 var popups = document.querySelectorAll('.popup')
 
-message.addEventListener("click", function(evt) {
+message.addEventListener('click', function(evt) {
 	evt.preventDefault();
 	overlay.classList.add('is-active');
 	form.classList.add('is-active');
 })
 
-mapButton.addEventListener("click", function(evt) {
+mapButton.addEventListener('click', function(evt) {
 	evt.preventDefault();
 	overlay.classList.add('is-active');
 	map.classList.add('is-active');
@@ -40,12 +40,12 @@ mapButton.addEventListener("click", function(evt) {
 var closebuttons = document.querySelectorAll('.close');
 
 	[].slice.call(closebuttons).forEach( function(button){
-		button.addEventListener("click", function(evt) {
+		button.addEventListener('click', function(evt) {
 			evt.preventDefault();
 			overlay.classList.remove('is-active');
 
 			[].slice.call(popups).forEach( function(popup) {
-				popup.classList.remove("is-active");
+				popup.classList.remove('is-active');
 			})
 		})
 })
